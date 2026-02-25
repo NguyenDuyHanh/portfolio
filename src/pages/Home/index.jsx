f4399df3ca133c5b3d1cdafa0ab0c72c989d3f5f
hanhnd <hanh27052004@gmail.com>
2026-02-25 11:37:23 +0700

    refactor: remove seo component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,10 @@
 import Hero from "./sections/Hero"
 import RecentBlog from "./sections/RecentBlog"
 import OpenSource from "./sections/OpenSource"
-import Seo from "../../components/Seo"
 
 const HomePage = () => {
   return (
     <div className="py-20 md:py-30">
-      <Seo
-        title="Hanh Nguyễn - Software Developer"
-        description="Welcome to Hanh Nguyen's personal portfolio website. Explore my projects, blog posts, and get to know more about me."
-        name="@hanhnguyen"
-        type="website"
-      />
       <Hero />
       <RecentBlog />
       <OpenSource />
